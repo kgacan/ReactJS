63e149cd65aa16775d6dc57167603ea5f471e79b
Kemal <kemal.gacan@gmail.com>
2019-10-11 10:11:09 +0200

    update styling with css module stylesheet

--- list-conditional-assignment/src/Char/Char.js
+++ list-conditional-assignment/src/Char/Char.js
@@ -1,16 +1,9 @@
 import React from "react";
+import classes from "./Char.module.css";
 
 const char = props => {
-  const style = {
-    display: "inline-block",
-    padding: "16px",
-    textAlign: "center",
-    margin: "16px",
-    border: "1px solid black"
-  };
-
   return (
-    <div style={style} onClick={props.clicked}>
+    <div className={classes.compFrame} onClick={props.clicked}>
       {props.letter}
     </div>
   );
